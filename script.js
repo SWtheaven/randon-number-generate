f8c85b4ca760c7a2c9067694ff8d1514429df1b9
SWtheaven <gspereira.dev@gmail.com>
2025-05-18 20:07:09 -0300

    update 1.01

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function generateNumber() {
     const inputMin = Math.ceil(document.querySelector(".input-min").value)
     const inputMax = Math.floor(document.querySelector(".input-max").value)
 
+    if (inputMin >= inputMax) {
+        alert("The minimum value must be less than the maximum value")
+    }
+
     const result = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
 
     alert(result)
   
 }
 
-
-
 generateButton[0].addEventListener("click", generateNumber)
+
